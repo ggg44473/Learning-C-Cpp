70e9e02ee366168b431542e32cb65ee9d00b213f
ggg44473 <ggg44473@gmail.com>
2018-09-13 05:12:10 +0800

    Release student_scores beta

--- student_scores/scores_function.h
+++ student_scores/scores_function.h
@@ -1,2 +1,249 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+struct _Student {
+  int id;
+  char name[10];
+  int math;
+  int english;
+  float average;
+};
+typedef struct _Student Student;
+
+struct _CheckResult {
+  int isExistedId;
+  int idPosition;
+};
+typedef struct _CheckResult CheckResult;
+
+int askExit();
+int askContinue();
+int getFileCount();
+void readFile();
+void saveFile();
+CheckResult checkId();
+void create();
+void list();
+CheckResult idSearch();
+void update();
+void delete();
+
+int askExit() {
+  char answer;
+  printf("Remember to SAVE!\nAre you sure to exit?[Y/N] ");
+  scanf("%c", &answer);
+  fflush(stdin);
+  if (answer == 'Y' || answer == 'y') {
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+int askContinue() {
+  char answer;
+  printf("Continue?[Y/N] ");
+  scanf("%c", &answer);
+  fflush(stdin);
+  if (answer == 'N' || answer == 'n') {
+    return askExit();
+  } else {
+    return 1;
+  }
+}
+
+int getFileCount(const char fname[]) {
+  int count = 0;
+  FILE *f = NULL;
+  Student buffer[1];
+
+  f = fopen(fname, "rb");
+  if (f == NULL) {
+    printf("\nFile open error.\n\n");
+  } else {
+    while (1) {
+      fread(buffer, sizeof(Student), 1, f);
+      // after read the eof, feof return true
+      if (feof(f)) {
+        break;
+      }
+      count++;
+    }
+  }
+  fclose(f);
+  return count;
+}
+
+void readFile(const char fname[], Student **pStudents, int *pCount) {
+  FILE *f = NULL;
+
+  *pCount = getFileCount(fname);
+
+  f = fopen(fname, "rb");
+  if (f == NULL) {
+    printf("\nFile open error.\n\n");
+  } else {
+    // need not to typecasting in c
+    *pStudents = malloc(sizeof(Student) * (*pCount));
+    fread((*pStudents), sizeof(Student), *pCount, f);
+  }
+  fclose(f);
+}
+
+void saveFile(const char fname[], Student **pStudents, int studentsCount) {
+  FILE *f = NULL;
+
+  f = fopen(fname, "wb");
+  if (f == NULL) {
+    printf("\nFile open error.\n\n");
+  } else {
+    fwrite(*pStudents, sizeof(Student), studentsCount, f);
+  }
+  fclose(f);
+
+  // It's dependent on readFile() with malloc().
+  // How to improve it?
+  free(*pStudents);
+}
+
+CheckResult checkId(int id, Student *students, int studentsCount) {
+  CheckResult result;
+
+  for (int i = 0; i < studentsCount; i++) {
+    if (id == students[i].id) {
+      result.isExistedId = 1;
+      result.idPosition = i;
+      return result;
+    } else {
+      result.isExistedId = 0;
+    }
+  }
+  return result;
+}
+
+void create(Student **pStudents, int *pCount) {
+  int id, math, english;
+  char name[10];
+  CheckResult result;
+  Student *temp;
+
+  do {
+    printf("id = ? ");
+    scanf("%d", &id);
+    fflush(stdin);
+
+    result = checkId(id, *pStudents, *pCount);
+
+    if (result.isExistedId) {
+      printf("The id has existed!\n");
+    }
+  } while (result.isExistedId);
+
+  printf("name = ? ");
+  scanf("%s", &name);
+  fflush(stdin);
+  printf("math = ? ");
+  scanf("%d", &math);
+  fflush(stdin);
+  printf("english = ? ");
+  scanf("%d", &english);
+  fflush(stdin);
+
+  temp = realloc(*pStudents, sizeof(Student) * (++*pCount));
+  if (!temp) {
+    printf("\nMemory ERROR!\n\n");
+  } else {
+    *pStudents = temp;
+
+    (*pStudents)[*pCount - 1].id = id;
+    strcpy((*pStudents)[*pCount - 1].name, name);
+    (*pStudents)[*pCount - 1].math = math;
+    (*pStudents)[*pCount - 1].english = english;
+    (*pStudents)[*pCount - 1].average = (math + english) / 2.0f;
+  }
+
+  printf("\nCreated!\n\n");
+}
+
+void list(Student *students, int studentsCount) {
+  printf("id\tname\tmath\tenglish\t\taverage\n");
+  for (int i = 0; i < studentsCount; i++) {
+    printf("%d\t%s\t%d\t%d\t\t%2.2f\n", students[i].id, students[i].name,
+           students[i].math, students[i].english, students[i].average);
+  }
+  printf("\n");
+}
+
+CheckResult idSearch(Student *students, int studentsCount) {
+  int id;
+  CheckResult result;
+
+  printf("id = ? ");
+  scanf("%d", &id);
+  fflush(stdin);
+
+  result = checkId(id, students, studentsCount);
+
+  if (result.isExistedId) {
+    printf("id\tname\tmath\tenglish\t\taverage\n");
+    printf("%d\t%s\t%d\t%d\t\t%2.2f\n", students[result.idPosition].id,
+           students[result.idPosition].name, students[result.idPosition].math,
+           students[result.idPosition].english,
+           students[result.idPosition].average);
+  } else {
+    printf("No id found...\n");
+  }
+
+  return result;
+}
+
+void update(Student *students, int studentsCount) {
+  CheckResult result;
+
+  result = idSearch(students, studentsCount);
+
+  if (result.isExistedId) {
+    printf("name = ? ");
+    scanf("%s", &students[result.idPosition].name);
+    fflush(stdin);
+    printf("math = ? ");
+    scanf("%d", &students[result.idPosition].math);
+    fflush(stdin);
+    printf("english = ? ");
+    scanf("%d", &students[result.idPosition].english);
+    fflush(stdin);
+    students[result.idPosition].average =
+        (students[result.idPosition].math +
+         students[result.idPosition].english) /
+        2.0f;
+    printf("\nUpdated!\n\n");
+  }
+}
+
+void delete (Student **pStudents, int *pCount) {
+  CheckResult result;
+  char answer;
+  Student *temp;
+
+  result = idSearch(*pStudents, *pCount);
+
+  if (result.isExistedId) {
+    printf("Are you sure to DELETE? [Y/N] ");
+    scanf("%c", &answer);
+    fflush(stdin);
+    if (answer == 'Y' || answer == 'y') {
+      for (int i = result.idPosition; i < *pCount - 1; i++) {
+        (*pStudents)[i] = (*pStudents)[i + 1];
+      }
+      temp = realloc(*pStudents, (*pCount - 1) * sizeof(Student));
+      if (!temp) {
+        printf("\nMemory ERROR!\n\n");
+      } else {
+        *pStudents = temp;
+        --*pCount;
+        printf("\nDeleted!\n\n");
+      }
+    }
+  }
+}
--- student_scores/scores_main.c
+++ student_scores/scores_main.c
@@ -1,231 +1,71 @@
 #include "scores_function.h"
 
-struct _Student {
-  int id;
-  char name[10];
-  int score_math;
-  int score_english;
-  float score_average;
-};
-typedef struct _Student Student;
-
-int getTotal();
-void create(); //離開程序後會多差一個奇怪的東西
-void pre_create();
-void read();
-void list();  // TODO sort
-void update();
-void delete ();
-int askContinue();
+#define FNAME "scores.bin"
 
 int main(int argc, char const *argv[]) {
   Student *students;
-  int total;
+  int studentsCount;
   char action;
   int is_continued;
-  char answer;
 
-  total = getTotal("scores.bin");
-  printf("total is %d\n", total);
+  readFile(FNAME, &students, &studentsCount);
 
+  system("cls");
   printf("Welcome! Please choose an action.\n");
-  is_continued = 1;
-  while (is_continued) {
-    printf("[A]create [B]read [C]list [D]update [E]delete [F]exit\n");
+
+  do {
+    printf(
+        "------------------------------------"
+        "-----------------------------------\n"
+        "[C]Create  [L]List  [I]IdSearch  [U]Update  "
+        "[D]Delete  [S]Save  [E]Exit\n");
+    scanf("%c", &action);
     fflush(stdin);
-    action = getchar();
-    fflush(stdin);
 
-    if (action == 'A') {
-      pre_create(students, "scores.bin");
-      is_continued = askContinue();
-
-    } else if (action == 'B') {
-      read(students, "scores.bin", total);
-      is_continued = askContinue();
-
-    } else if (action == 'C') {
-      list(students, total, "scores.bin");
-      is_continued = askContinue();
-
-    } else if (action == 'D') {
-      is_continued = askContinue();
-
-    } else if (action == 'E') {
-      is_continued = askContinue();
-
-    } else if (action == 'F') {
-      is_continued = 0;
-
-    } else {
-      printf("Command not found.\n");
-      is_continued = 1;
+    switch (action) {
+      case 'C':
+      case 'c':
+        create(&students, &studentsCount);
+        break;
+      case 'L':
+      case 'l':
+        list(students, studentsCount);
+        break;
+      case 'I':
+      case 'i':
+        idSearch(students, studentsCount);
+        break;
+      case 'U':
+      case 'u':
+        update(students, studentsCount);
+        break;
+      case 'D':
+      case 'd':
+        delete(&students, &studentsCount);
+        break;
+      case 'S':
+      case 's':
+        saveFile(FNAME, &students, studentsCount);
+        readFile(FNAME, &students, &studentsCount);
+        break;
+      case 'E':
+      case 'e':
+        is_continued = askExit();
+        break;
+      default:
+        printf("Wrong action!\n");
+        break;
     }
+    if (action != 'E' && action != 'e') {
+      is_continued = askContinue();
+    }
+  } while (is_continued);
+
+  if (students) {
+    free(students);
   }
 
-  printf("BYE BYE :)");
+  printf("\nBYE BYE :)\n");
+
   return 0;
-}
-
-int askContinue() {
-  char answer;
-  printf("Continue?[Y/N] ");
-  fflush(stdin);
-  scanf("%c", &answer);
-  if (answer == 'N') {
-    return 0;
-  } else {
-    return 1;
-  }
-}
-int getTotal(char file[]) {
-  FILE *f;
-  Student student[1];
-  int total = 0;
-
-  f = fopen(file, "rb");
-  if (f == NULL) {
-    printf("File open error.\n");
-  } else {
-    while (!feof(f)) {
-      fread(student, sizeof(Student), 1, f) == 1;
-      total++;
-    }
-  }
-  fclose(f);
-  return total;
-}
-
-void create(Student *students, int s_number, char file[]) {
-  FILE *f = NULL;
-
-  f = fopen(file, "ab");
-  if (f == NULL) {
-    printf("File open error.\n");
-  }
-
-  fwrite(students, sizeof(Student), s_number, f);
-
-  fclose(f);
-}
-
-void pre_create(Student *students, char file[]) {
-  int s_number;
-  printf("How many students do you want to create? ");
-  scanf("%d", &s_number);
-
-  students = (Student *)malloc(sizeof(Student) * s_number);
-
-  for (int i = 0; i < s_number; i++) {
-    printf("id = ? ");
-    scanf("%d", &students[i].id);
-    fflush(stdin);
-    printf("name = ? ");
-    scanf("%s", students[i].name);
-    fflush(stdin);
-    printf("math = ? ");
-    scanf("%d", &students[i].score_math);
-    fflush(stdin);
-    printf("english = ? ");
-    scanf("%d", &students[i].score_english);
-    fflush(stdin);
-    students[i].score_average =
-        (students[i].score_math + students[i].score_english) / 2.0f;
-  }
-
-  create(students, s_number, file);
-
-  free(students);
-}
-
-void read(Student *students, char file[], int total) {
-  int s_number;
-  int *ids;
-  FILE *f = NULL;
-
-  printf("How many students do you want to read? ");
-  scanf("%d", &s_number);
-
-  ids = (int *)malloc(sizeof(int) * s_number);
-
-  printf(
-      "Which id do you want to read?\nInput one and press enter for the "
-      "next\n");
-
-  printf("First? ");
-  scanf("%d", &ids[0]);
-  for (int i = 1; i < s_number; i++) {
-    printf("Next? ");
-    scanf("%d", &ids[i]);
-  }
-
-  f = fopen(file, "rb");
-  if (f == NULL) {
-    printf("File open error.\n");
-  }
-  students = (Student *)malloc(sizeof(Student) * total);
-  fread(students, sizeof(Student), total, f);
-
-  printf("id\tname\tmath\tenglish\taverage\t\n");
-  for (int i = 0; i < s_number; i++) {
-    for (int j = 0; j < total; j++) {
-      if (ids[i] == students[j].id) {
-        printf("%d\t%s\t%d\t%d\t%f\n", students[j].id, students[j].name,
-               students[j].score_math, students[j].score_english,
-               students[j].score_average);
-      }
-    }
-  }
-
-  fclose(f);
-  free(ids);
-  free(students);
-}
-
-void list(Student *students, int total, char file[]) {
-  FILE *f = NULL;
-
-  f = fopen(file, "rb");
-  if (f == NULL) {
-    printf("File open error.\n");
-  }
-  students = (Student *)malloc(sizeof(Student) * total);
-
-  fread(students, sizeof(Student), total, f);
-
-  printf("id\tname\tmath\tenglish\taverage\t\n");
-  for (int i = 0; i < total; i++) {
-    printf("%d\t%s\t%d\t%d\t%f\n", students[i].id, students[i].name,
-           students[i].score_math, students[i].score_english,
-           students[i].score_average);
-  }
-
-  fclose(f);
-  free(students);
-}
-
-void update(Student *students, int s_number, char file[]) {
-  FILE *f = NULL;
-
-  f = fopen(file, "ab");
-  if (f == NULL) {
-    printf("File open error.\n");
-  }
-
-  fwrite(students, sizeof(Student), s_number, f);
-
-  fclose(f);
-}
-
-void delete (Student *students, int s_number, char file[]) {
-  FILE *f = NULL;
-
-  f = fopen(file, "ab");
-  if (f == NULL) {
-    printf("File open error.\n");
-  }
-
-  fwrite(students, sizeof(Student), s_number, f);
-
-  fclose(f);
 }
