ec869e8587dbfeea319f6d4169ad267e697d7467
Sean Wang <ggg44473@gmail.com>
2018-09-19 18:25:32 +0800

    Update FromOneAddToN.c

--- easy_start/FromOneAddToN.c
+++ easy_start/FromOneAddToN.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 
-int F(int n)
-{
-    if (n == 1)
-    {
-        return 1;
-    }
-    else
-    {
-        return n + F(n - 1);
-    }
+int F(int n) {
+  if (n == 1) {
+    return 1;
+  } else {
+    return n + F(n - 1);
+  }
 }
 
-int main(int argc, char const *argv[])
-{
-    int n;
-    printf("N = ? ");
-    scanf("%d", &n);
-    fflush(stdin);
-    printf("1+2+...+n = %d", F(n));
-    return 0;
+int main(int argc, char const *argv[]) {
+  int n;
+  printf("N = ? ");
+  scanf("%d", &n);
+  fflush(stdin);
+  printf("1+2+...+%d = %d", n, F(n));
+  return 0;
 }
